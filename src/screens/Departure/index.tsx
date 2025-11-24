e0a644f121b9ab6f745f7ea79588ed229cf879bf
lubnniamorais <lubnnia@gmail.com>
2025-11-24 10:21:53 -0300

    chore: Include ref property

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,10 +1,42 @@
+import { useRef } from 'react';
+import { TextInput } from 'react-native';
+import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
-import { Container } from './styles';
+import { LicensePlateInput } from '../../components/LicensePlateInput';
+import { TextAreaInput } from '../../components/TextAreaInput';
+import { Container, Content } from './styles';
 
 export function Departure() {
+  // Criando refs para os inputs, onde a tipagem é um TextInput
+  const descriptionRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    console.log('Ok');
+  }
+
   return (
     <Container>
       <Header title='Saída' />
+
+      <Content>
+        <LicensePlateInput
+          label='Placa do veículo'
+          placeholder='ABC-1234'
+          onSubmitEditing={() => descriptionRef.current?.focus()} // Aqui vamos colocar o foco no input de finalidade
+          returnKeyType='next' // Mudando o ícone do teclado
+        />
+
+        <TextAreaInput
+          ref={descriptionRef}
+          label='Finalidade'
+          placeholder='Vou utilizar o veículo para...'
+          onSubmitEditing={handleDepartureRegister}
+          returnKeyType='send'
+          submitBehavior='submit' // Propriedade que tem o mesmo efeito de enviar do botão
+        />
+
+        <Button title='Registrar saída' onPress={handleDepartureRegister} />
+      </Content>
     </Container>
   );
 }
--- src/screens/Departure/styles.ts
+++ src/screens/Departure/styles.ts
@@ -4,3 +4,10 @@ export const Container = styled.View`
   flex: 1;
   background-color: red;
 `;
+
+export const Content = styled.View`
+  flex: 1;
+  gap: 16px;
+  padding: 32px;
+  margin-top: 16px;
+`;
