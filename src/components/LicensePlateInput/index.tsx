4f621ecbc8db9721ce9651c458003a13be015b0b
lubnniamorais <lubnnia@gmail.com>
2025-11-27 15:56:01 -0300

    feat: Create regex to licensePlate validate

--- src/components/LicensePlateInput/index.tsx
+++ src/components/LicensePlateInput/index.tsx
@@ -1,24 +1,33 @@
-import { TextInputProps } from 'react-native';
+import { forwardRef } from 'react';
+import { TextInput, TextInputProps } from 'react-native';
+
 import { useTheme } from 'styled-components/native';
+
 import { Container, Input, Label } from './styles';
 
 type Props = TextInputProps & {
   label: string;
 };
 
-export function LicensePlateInput({ label, ...rest }: Props) {
-  const { COLORS } = useTheme();
+const LicensePlateInput = forwardRef<TextInput, Props>(
+  ({ label, ...rest }, ref) => {
+    const { COLORS } = useTheme();
 
-  return (
-    <Container>
-      <Label>{label}</Label>
+    return (
+      <Container>
+        <Label>{label}</Label>
 
-      <Input
-        maxLength={7}
-        autoCapitalize='characters'
-        placeholderTextColor={COLORS.GRAY_400}
-        {...rest}
-      />
-    </Container>
-  );
-}
+        <Input
+          ref={ref}
+          placeholder='AAA-0000'
+          maxLength={7}
+          autoCapitalize='characters'
+          placeholderTextColor={COLORS.GRAY_400}
+          {...rest}
+        />
+      </Container>
+    );
+  }
+);
+
+export { LicensePlateInput };
--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,42 +1,73 @@
-import { useRef } from 'react';
-import { TextInput } from 'react-native';
+import { useRef, useState } from 'react';
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+} from 'react-native';
+
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
 import { TextAreaInput } from '../../components/TextAreaInput';
+import { licensePlateValidate } from '../../utils/licensePlateValidate';
 import { Container, Content } from './styles';
 
+const keyboardAvoidingViewBehavior =
+  Platform.OS === 'android' ? 'height' : 'position';
+
 export function Departure() {
+  const [description, setDescription] = useState('');
+  const [licensePlate, setLicensePlate] = useState('');
+
   // Criando refs para os inputs, onde a tipagem é um TextInput
   const descriptionRef = useRef<TextInput>(null);
+  const licensePlateRef = useRef<TextInput>(null);
 
   function handleDepartureRegister() {
-    console.log('Ok');
+    if (!licensePlateValidate(licensePlate)) {
+      licensePlateRef.current?.focus();
+      return Alert.alert(
+        'Placa inválida',
+        'A placa é inválida. Por favor, informe a plca corretado veículo'
+      );
+    }
   }
 
   return (
     <Container>
       <Header title='Saída' />
 
-      <Content>
-        <LicensePlateInput
-          label='Placa do veículo'
-          placeholder='ABC-1234'
-          onSubmitEditing={() => descriptionRef.current?.focus()} // Aqui vamos colocar o foco no input de finalidade
-          returnKeyType='next' // Mudando o ícone do teclado
-        />
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={keyboardAvoidingViewBehavior}
+      >
+        <ScrollView>
+          <Content>
+            <LicensePlateInput
+              ref={licensePlateRef}
+              label='Placa do veículo'
+              placeholder='ABC-1234'
+              onSubmitEditing={() => descriptionRef.current?.focus()} // Aqui vamos colocar o foco no input de finalidade
+              returnKeyType='next' // Mudando o ícone do teclado
+              onChangeText={setLicensePlate}
+            />
 
-        <TextAreaInput
-          ref={descriptionRef}
-          label='Finalidade'
-          placeholder='Vou utilizar o veículo para...'
-          onSubmitEditing={handleDepartureRegister}
-          returnKeyType='send'
-          submitBehavior='submit' // Propriedade que tem o mesmo efeito de enviar do botão
-        />
+            <TextAreaInput
+              ref={descriptionRef}
+              label='Finalidade'
+              placeholder='Vou utilizar o veículo para...'
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType='send'
+              submitBehavior='submit' // Propriedade que tem o mesmo efeito de enviar do botão
+              onChangeText={setDescription}
+            />
 
-        <Button title='Registrar saída' onPress={handleDepartureRegister} />
-      </Content>
+            <Button title='Registrar saída' onPress={handleDepartureRegister} />
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
