b09bf90a20c9968ea8b756685242dd710b8d2ab4
lubnniamorais <lubnnia@gmail.com>
2026-02-13 14:11:49 -0300

    chore: Add function to startLocationTask and stopLocationTask in background

--- src/tasks/backgroundLocationTask.ts
+++ src/tasks/backgroundLocationTask.ts
@@ -1,3 +1,9 @@
+import {
+  Accuracy,
+  hasStartedLocationUpdatesAsync,
+  startLocationUpdatesAsync,
+  stopLocationUpdatesAsync,
+} from 'expo-location';
 import * as TaskManager from 'expo-task-manager';
 
 // Exportamos a tarefa, pois iremos aproveitar em outros lugares
@@ -5,7 +11,7 @@ import * as TaskManager from 'expo-task-manager';
 export const BACKGROUND_TASK_NAME = 'location-tracking';
 
 // Aqui estamos definindo a tarefa
-TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
+TaskManager.defineTask(BACKGROUND_TASK_NAME, async ({ data, error }: any) => {
   try {
     if (error) {
       throw error;
@@ -24,3 +30,37 @@ TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
     console.log(error);
   }
 });
+
+export async function startLocationTask() {
+  try {
+    const hasStarted =
+      await hasStartedLocationUpdatesAsync(BACKGROUND_TASK_NAME);
+
+    // Se a tarefa ja foi iniciada, vamos parar ela
+    if (hasStarted) {
+      await stopLocationTask();
+    }
+
+    // A função startLocationUpdateAsync fica obtendo a atualização da locatização do usuário
+    await startLocationUpdatesAsync(BACKGROUND_TASK_NAME, {
+      accuracy: Accuracy.Highest,
+      distanceInterval: 1,
+      timeInterval: 1000,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function stopLocationTask() {
+  try {
+    const hasStarted =
+      await hasStartedLocationUpdatesAsync(BACKGROUND_TASK_NAME);
+
+    if (hasStarted) {
+      await stopLocationUpdatesAsync(BACKGROUND_TASK_NAME);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
